ea1a8ec468ca39ef35813eb55d1e25d59b2926f8
aman <amansetu92@gmail.com>
2023-02-23 11:13:41 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiply two integers
+ * @x: The first number to multiply
+ * @y: The second number to mutiply
+ * Return: The result of mutiplication between x param and y param
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
