b3a62658c02000ee135ba5a9ca13a0e8aaa115db
aman <amansetu92@gmail.com>
2023-03-03 12:24:57 -0800

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - Copares two strings to two strings
+ * @s1: A pointer to the first string to be compared
+ * @s2: A pointer to the second string to be compared
+ * Return: If str1 < str2, the negative difference of the first unmatched
+ * If str1 == str2, 0.
+ * If str1 > str2, the postive difference of the first unmatched
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+	s1++;
+	s2++;
+	}
+	return (*s1 - *s2);
+}
